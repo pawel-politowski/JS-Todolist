5be1eac66ad6e93e50544399971f57e8e77fcfaa
Paweł Politowski <matelly@poczta.onet.pl>
2020-05-25 21:42:14 +0200

    Added save to local storage

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const filterOption = document.querySelector('.filter-todo');
 const searchBtn = document.querySelector('.search-button');
 const searchInput = document.querySelector('.todo-search');
 
+document.addEventListener('DOMContentLoaded', getTodos)
 todoButton.addEventListener('click', addTodo);
 filterOption.addEventListener('click', filterTodo);
 searchBtn.addEventListener("click", searchTask);
@@ -23,6 +24,8 @@ function addTodo(event) {
     newTodo.innerHTML = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    //add todo to local storage
+    saveToLocalStorage(todoInput.value);
     //create mark button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -103,6 +106,58 @@ function removeClassFound (){
 }
 
 removeClassFound();
+
+function saveToLocalStorage(todo) {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+function getTodos() {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo){
+         //create div todo
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //create li
+    const newTodo = document.createElement('li');    
+    newTodo.innerHTML = todo;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);  
+    //create mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //create trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //append to list
+    todoList.appendChild(todoDiv);
+    })
+}
+
+function removeLocalStorageTodos(todo){
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    
+}
    
 
 
