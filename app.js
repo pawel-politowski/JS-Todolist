1e0749ea9aff6d1e77a157feb0156e3f01662eae
Paweł Politowski <matelly@poczta.onet.pl>
2020-05-25 21:18:58 +0200

    Added search by text

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 const trashButton = document.querySelector('.trash-btn');
 const filterOption = document.querySelector('.filter-todo');
+const searchBtn = document.querySelector('.search-button');
+const searchInput = document.querySelector('.todo-search');
 
 todoButton.addEventListener('click', addTodo);
 filterOption.addEventListener('click', filterTodo);
+searchBtn.addEventListener("click", searchTask);
 
 function addTodo(event) {
     event.preventDefault();
@@ -80,4 +83,26 @@ function filterTodo(event) {
     }
     });
 };
+
+function searchTask(event){
+    event.preventDefault();
+    searchTxt = searchInput.value.toLowerCase();
+    let listOfLi = [...document.querySelectorAll(".todo")];    
+    listOfLi.forEach(element => {
+        if (element.innerText.toLowerCase().indexOf(searchTxt) > -1){
+            element.classList.remove('found') ;
+        } else {
+            element.classList.add('found');    
+}}
+)}
+
+function removeClassFound (){
+    if (searchInput.value = ""){
+        li.classList.remove('found')
+    }
+}
+
+removeClassFound();
+   
+
 
